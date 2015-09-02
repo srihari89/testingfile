033339c9c1ffd785d985d499548c2fb2ec6fe5b3
Srihari <srihari@cgvakindia.com>
2015-09-02 20:23:34 +0530

    My first commit

---
+++ info.php
@@ -0,0 +1,6 @@
+<?php
+
+phpinfo();
+//https://mscshankar.wordpress.com/2012/09/29/magento-interview-questions-and-answers/
+//http://magentoabid.blogspot.in/p/magento-interview-questions-and-answers.html
+?>
